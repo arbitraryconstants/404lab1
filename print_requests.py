20f7f1ddc26f0f7d38083a3cb784b439d6628bad
arbitrarycontstants <mclaffer@ualberta.ca>
2017-01-12 12:16:57 -0700

    added email to git config

--- print_requests.py
+++ print_requests.py
@@ -7,5 +7,6 @@ print requests.__version__
 response = requests.get("https://raw.githubusercontent.com/arbitraryconstants/404lab1/master/print_requests.py")
 
 print response.text
+
 print response.status_code
 
