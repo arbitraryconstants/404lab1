4b0f56d83ea0a7a40960167a9c6b9d0f0d338567
arbitrarycontstants <mclaffer@cs.ualberta.ca>
2017-01-12 12:12:13 -0700

    testing username

--- print_requests.py
+++ print_requests.py
@@ -7,6 +7,5 @@ print requests.__version__
 response = requests.get("https://raw.githubusercontent.com/arbitraryconstants/404lab1/master/print_requests.py")
 
 print response.text
-
 print response.status_code
 
